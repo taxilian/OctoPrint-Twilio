cb5dc13508f0aa3bdc7eac9ab8a22669ac827fe8
Levi Blaney <shadycuz@gmail.com>
2018-01-07 22:07:05 -0500

    added requests to requirements

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/anoved/OctoPrint-EmailNotifier"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ['twilio>=6','phonenumbers']
+plugin_requires = ['twilio>=6','phonenumbers','requests']
 
 # Additional package data to install for this plugin. The subfolders "templates", "static" and "translations" will
 # already be installed automatically if they exist.
